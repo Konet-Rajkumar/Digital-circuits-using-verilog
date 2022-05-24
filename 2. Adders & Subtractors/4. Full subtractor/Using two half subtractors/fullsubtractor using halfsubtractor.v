20c01a1c3a8aa2d3b5d6cb835a40146e33377f91
Koneti Raj kumar <106098401+Konet-Rajkumar@users.noreply.github.com>
2022-05-24 21:31:26 +0530

    Update fullsubtractor using halfsubtractor.v

--- 2. Adders & Subtractors/4. Full subtractor/Using two half subtractors/fullsubtractor using halfsubtractor.v
+++ 2. Adders & Subtractors/4. Full subtractor/Using two half subtractors/fullsubtractor using halfsubtractor.v
@@ -1,1 +1,10 @@
+module S4fullsubtractor(a,b,c,diff,borrow
+    );
+input a,b,c;
+output diff,borrow;
+wire diff,d1,borrow,b1,b2;
+S3halfsubtractor h1(a,b,d1,b1);
+S3halfsubtractor h2(d1,c,diff,b2);
+Sor o1(b1,b2,borrow);
+endmodule
 
