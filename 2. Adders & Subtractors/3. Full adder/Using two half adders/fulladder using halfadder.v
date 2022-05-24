a9fc8ccb17ac20a4ebf6e30d7d0e24ad6545ff60
Koneti Raj kumar <106098401+Konet-Rajkumar@users.noreply.github.com>
2022-05-24 21:35:54 +0530

    Update fulladder using halfadder.v

--- 2. Adders & Subtractors/3. Full adder/Using two half adders/fulladder using halfadder.v
+++ 2. Adders & Subtractors/3. Full adder/Using two half adders/fulladder using halfadder.v
@@ -1,1 +1,10 @@
+module S2fulladder(a,b,c,sum,carry
+    );
+input a,b,c;
+output sum,carry;
+wire sum,s,carry0,carry1,carry;
+S1halfadder h1(a,b,s,carry0);
+S1halfadder h2(s,c,sum,carry1);
+Sor o1(carry0,carry1,carry);
+  endmodule
 
