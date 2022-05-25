054bfa65fb24b1f5d98a0329c7396c253cf40585
Koneti Raj kumar <106098401+Koneti-Rajkumar@users.noreply.github.com>
2022-05-25 09:19:46 +0530

    Add files via upload

---
+++ 9. Counters/Asynchronous/Decade counter/Behavioral modelling/Decade_counter.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:29:05 02/10/2021 
+// Design Name: 
+// Module Name:    Decade_counter 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Decade_counter(count,clk,rst);
+input clk,rst;
+output reg [3:0]count;
+always @(posedge clk)
+begin
+if(rst||count==4'b1001)
+count=4'b0000;
+else
+count=count+1;
+end
+endmodule 
