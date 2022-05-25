8cdc2bd9c5554149528513571cf1321fb0e163c1
Koneti Raj kumar <106098401+Koneti-Rajkumar@users.noreply.github.com>
2022-05-25 08:17:10 +0530

    Add files via upload

---
+++ 8. Flip Flops/4. T Flip Flop/3. D to T Flip Flop/Structural modelling/Dff_to_Tff.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    07:09:00 02/07/2021 
+// Design Name: 
+// Module Name:    Dff_to_Tff 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Dff_to_Tff(q,qb,t,clk,rst);
+input t;
+input clk,rst;
+output wire q,qb;
+Dff a1(q,qb,t^q,clk,rst);
+endmodule
