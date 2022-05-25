1008ddf8e92ded8889de8953d7e4ce085dc9be29
Koneti Raj kumar <106098401+Koneti-Rajkumar@users.noreply.github.com>
2022-05-25 07:55:35 +0530

    Add files via upload

---
+++ 8. Flip Flops/1. SR Flip Flop/4. T to SR Flip FLop/Structural modelling/Tff_to_SRff.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    06:46:54 02/07/2021 
+// Design Name: 
+// Module Name:    Tff_to_SRff 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Tff_to_SRff(q,qb,s,r,clk,rst);
+input s,r;
+input clk,rst;
+output wire q,qb;
+Tff a1(q,qb,((s&qb)|(r&q)),clk,rst);
+endmodule
