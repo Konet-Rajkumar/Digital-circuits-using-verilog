26dfc46bcdf385aaed9c24639672a8791a067a3a
Koneti Raj kumar <106098401+Koneti-Rajkumar@users.noreply.github.com>
2022-05-25 08:17:13 +0530

    Add files via upload

---
+++ 8. Flip Flops/4. T Flip Flop/4. JK to T Flip Flop/Structural modelling/JKff_to_Tff.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    07:05:44 02/07/2021 
+// Design Name: 
+// Module Name:    JKff_to_Tff 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module JKff_to_Tff(q,qb,t,clk,rst);
+input t;
+input clk,rst;
+output wire q,qb;
+JKff a1(q,qb,t,t,clk,rst);
+endmodule
