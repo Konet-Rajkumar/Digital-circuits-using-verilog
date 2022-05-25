26967f7293055a03cff3d5acde2e71b42e7e9bff
Koneti Raj kumar <106098401+Koneti-Rajkumar@users.noreply.github.com>
2022-05-25 09:09:43 +0530

    Add files via upload

---
+++ 9. Counters/Synchronous/Down counter/Structural modelling/sync_downcounter.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:27:09 02/07/2021 
+// Design Name: 
+// Module Name:    sync_downcounter 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module sync_downcounter(q,qb,clk,rst,t);
+input clk,rst;
+input t;
+output [3:0]q,qb;
+TFF t0(q[0],qb[0],clk,rst,t);
+TFF t1(q[1],qb[1],clk,rst,qb[0]);
+TFF t2(q[2],qb[2],clk,rst,(qb[1]&qb[0]));
+TFF t3(q[3],qb[3],clk,rst,((qb[1]&qb[0])&qb[2]));
+endmodule
