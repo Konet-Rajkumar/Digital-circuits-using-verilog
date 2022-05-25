70767e36bc09fbad663236a8a9cd056cb2d686ee
Koneti Raj kumar <106098401+Koneti-Rajkumar@users.noreply.github.com>
2022-05-25 08:11:51 +0530

    Add files via upload

---
+++ 8. Flip Flops/3. JK Flip Flop/2. SR to JK Flip FLop/Structural modelling/SRff_to_JKff.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    06:48:37 02/07/2021 
+// Design Name: 
+// Module Name:    SRff_to_JKff 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module SRff_to_JKff(q,qb,j,k,clk,rst);
+input j,k;
+input clk,rst;
+output wire q,qb;
+SRff a1(q,qb,j&qb,k&q,clk,rst);
+endmodule 
