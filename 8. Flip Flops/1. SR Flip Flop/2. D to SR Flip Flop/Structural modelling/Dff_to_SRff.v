eaabd0c048e9da64a04a5f63463317fdd9ea9f2f
Koneti Raj kumar <106098401+Koneti-Rajkumar@users.noreply.github.com>
2022-05-25 07:55:41 +0530

    Add files via upload

---
+++ 8. Flip Flops/1. SR Flip Flop/2. D to SR Flip Flop/Structural modelling/Dff_to_SRff.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    07:09:49 02/07/2021 
+// Design Name: 
+// Module Name:    Dff_to_SRff 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Dff_to_SRff(q,qb,s,r,clk,rst);
+input s,r;
+input clk,rst;
+output wire q,qb;
+Dff a1(q,qb,(s|(~r&q)),clk,rst);
+endmodule
