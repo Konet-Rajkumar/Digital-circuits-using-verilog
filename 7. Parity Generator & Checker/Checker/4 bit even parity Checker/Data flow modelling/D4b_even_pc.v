4b1b16bc2f108322bd7c40cd57ed56422344b939
Koneti Raj kumar <106098401+Koneti-Rajkumar@users.noreply.github.com>
2022-05-25 07:39:16 +0530

    Add files via upload

---
+++ 7. Parity Generator & Checker/Checker/4 bit even parity Checker/Data flow modelling/D4b_even_pc.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:50:06 02/07/2021 
+// Design Name: 
+// Module Name:    D4b_even_pc 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module D4b_even_pc(e,o);
+input [3:0]o;
+output wire e;
+assign e=o[0]^o[1]^o[2]^o[3];
+endmodule
