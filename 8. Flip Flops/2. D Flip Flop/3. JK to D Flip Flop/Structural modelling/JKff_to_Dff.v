1340afa15d5ee6592ca03372b1bf14a3a3dbb068
Koneti Raj kumar <106098401+Koneti-Rajkumar@users.noreply.github.com>
2022-05-25 08:02:37 +0530

    Add files via upload

---
+++ 8. Flip Flops/2. D Flip Flop/3. JK to D Flip Flop/Structural modelling/JKff_to_Dff.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:27:56 01/20/2021 
+// Design Name: 
+// Module Name:    JKff_to_Dff 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module JKff_to_Dff(q,qb,d,clk,rst);
+input d;
+input clk,rst;
+output wire q,qb;
+JKff a1(q,qb,d,~d,clk,rst);
+endmodule
